36d4b0520cb29abd7b408cbc7f859d714ca52dba
xw0 <weixiang77@gmail.com>
2024-05-05 22:18:38 -0700

    support mps device

--- Chapter23/train.py
+++ Chapter23/train.py
@@ -50,8 +50,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", required=True, help="Name of the run")
     parser.add_argument("--cuda", default=False, action="store_true", help="Enable CUDA")
+    parser.add_argument("--mps", default=False, action="store_true", help="Enable MPS")
     args = parser.parse_args()
-    device = torch.device("cuda" if args.cuda else "cpu")
+    if args.mps:
+        device = torch.device("mps")
+    else:
+        device = torch.device("cuda" if args.cuda else "cpu")
 
     saves_path = os.path.join("saves", args.name)
     os.makedirs(saves_path, exist_ok=True)
